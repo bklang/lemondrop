168a3b25fe1fc53370829efb905d89ea5cbd941e
Mehak <mehakkahlon@gmail.com>
2013-12-18 16:26:20 +0530

    [mehak] able to connect if password not set for redis server

--- lib/lemondrop/plugin.rb
+++ lib/lemondrop/plugin.rb
@@ -9,7 +9,7 @@ class Lemondrop::Plugin < Adhearsion::Plugin
   config :lemondrop do
     uri         ''         , :desc => 'URI to the Redis instance. Use this or specify each piece of connection information separately below.'
     username    ''         , :desc => 'valid database username'
-    password    ''         , :desc => 'valid database password'
+    password    nil        , :desc => 'valid database password'
     host        'localhost', :desc => 'host where the database is running'
     port        6379       , :desc => 'port where the database is listening'
     socket      ''         , :desc => 'path to Unix socket where Redis is listening (Optional; to use, set host and port to nil)'
